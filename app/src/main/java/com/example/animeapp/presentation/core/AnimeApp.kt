fc4f72a656a585a4f2c5e702062591643310b194
Vitalii Lebedynskyi <vetal.lebed@gmail.com>
2023-12-20 15:47:39 +0100

    Added scroll behaviour

--- app/src/main/java/com/example/animeapp/presentation/core/AnimeApp.kt
+++ app/src/main/java/com/example/animeapp/presentation/core/AnimeApp.kt
@@ -1,6 +1,7 @@
 package com.example.animeapp.presentation.core
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.example.animeapp.data.AnimeApi
 import com.example.animeapp.data.AnimeApiDataSource
 import com.example.animeapp.data.AppDatabase
@@ -9,7 +10,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
-class AnimeApp : Application() {
+class AnimeApp : MultiDexApplication() {
     lateinit var db: AppDatabase
     lateinit var animeApi: AnimeApiDataSource
     lateinit var mangaApi: MangaApiDataSource
